d015f54466d0d48b25c10a3837ef89410c9a0cdd
lucapintimalli <luca.pintimalli99@gmail.com>
2024-07-23 15:29:32 +0200

    creazione models camere

---
+++ Albergo/Albergo/Models/Camere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Albergo.Models
+{
+	public class Camere
+	{
+        [Key]
+        public int Numero { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Descrizione { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Tipologia { get; set; }
+    }
+}
+
