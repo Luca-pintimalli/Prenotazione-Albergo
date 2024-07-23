5615b683f8127bf59049df1ca5b898a47ee54f87
lucapintimalli <luca.pintimalli99@gmail.com>
2024-07-23 15:38:32 +0200

    Aggiunta models Camera + SqlConnectionService

---
+++ Albergo/Albergo/Services/ServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Albergo.Services
+{
+	public abstract class ServiceBase
+	{
+		//METODO CHE RESTITUISCE LA CONNESIONE AL SERVER 
+		protected abstract DbConnection GetConnection();
+
+		//METODO CHE CREA UN COMANDO
+		protected abstract DbCommand GetCommand(string commandText);
+	}
+}
+
---
+++ Albergo/Albergo/Services/SqlServerServiceBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Albergo.Services
+{
+	public class SqlServerServiceBase : ServiceBase
+    {
+
+        private SqlConnection _connection;
+
+		public SqlServerServiceBase(IConfiguration config )
+		{
+            _connection = new SqlConnection(config.GetConnectionString("AppDb"));
+		}
+
+       
+
+        protected override DbCommand GetCommand(string commandText)
+        {
+            return new SqlCommand(commandText, _connection);
+        }
+
+        protected override DbConnection GetConnection()
+        {
+            return _connection;
+        }
+    }
+}
+
